c46899f6c4d7dfb21c2ad11d0601ad2b2b350353
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2010-11-21 23:33:03 +0000

    Started skeleton of code.

--- src/sfsm.c
+++ src/sfsm.c
@@ -9,10 +9,29 @@
 	Please see LICENSE file.
 */
 #include <stdio.h>
-#include <stdlib.h>
 #include "xplatform.h"
 
-int main(void) {
-	
+void loadStateMachine(char *file);
+
+FILE *pFile;
+
+int main(int argc, char *argv[]) {
+	if(argc < 2) {
+		printf("SFSM (Scriptable Finite State Machine)");
+		#ifdef _WIN32
+		printf("\nUsage: sfsm.exe <fsm script>\n");
+		#else 
+		printf("\nUsage: sfsm <fsm script>\n");
+		#endif
+	}
+	else loadStateMachine(argv[1]);
 	return 0;
+}
+
+void loadStateMachine(char *file) {
+	char *line;
+	pFile = fopen(file, "r");
+	if(pFile != NULL) {
+		fgets(
+	}
 }
